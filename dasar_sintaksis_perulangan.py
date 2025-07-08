b3b251b18348fcb1295c8a7946e7582b56cdd143
MurphyJK <jeleklo82@gmail.com>
2025-07-08 13:10:51 +0800

    baris ke 12 beda sama pak eko. pak eko : for jumlah_buku_yang_sudah_dibaca in range (1, jumlah_buku+1): saya : for jumlah_buku_yang_sudah_dibaca in range (1, jumlah_buku_yang_sudah_dibaca+1):
    beda karena cari berapa buku yang sudah dibaca bukan jumlah buku.

---
+++ dasar_sintaksis_perulangan.py
@@ -0,0 +1,15 @@
+"""
+# Program perulangan membaca buku
+"""
+
+jumlah_buku = 100
+print("Ibu berkata, \"baca semua buku bukumu\"")
+
+jumlah_buku_yang_sudah_dibaca = 10
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+print("Dengan for")
+for jumlah_buku_yang_sudah_dibaca in range (1, jumlah_buku_yang_sudah_dibaca+1):
+    print(f"Buke ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+    print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
